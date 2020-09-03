3a70eeda5c7b5609b7640be960ff4743b23f5ee8
mirtauhid <67647821+mirtauhid@users.noreply.github.com>
2020-09-04 01:29:50 +0600

    done with material & axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './Components/Header/Header';
+import News from './Components/News/News';
+import Footer from './Components/Footer/Footer';
+import axios from 'axios';
 
 function App() {
+  const [news, setNews] = useState([]);
+  const url = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-03&sortBy=publishedAt&apiKey=3a1a4d9585284dc39458e3e0ffee368a`;
+  useEffect(() => {
+    axios.get(url)
+    .then(res => {
+      const newsData = res.data.articles;
+      console.log(newsData);
+      setNews(newsData.slice(0,18))
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      {
+        news.map((news) => <News news={news}></News>)
+      }
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p><small>copyright © 2020 - Tauhid</small></p>
+        </div>
+    )
+};
+
+export default Footer;
