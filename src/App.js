694ecae94395059f9535edad36d385e158cf4458
mirtauhid <67647821+mirtauhid@users.noreply.github.com>
2020-10-11 02:03:36 +0600

    api upgrade

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import axios from 'axios';
 
 function App() {
   const [news, setNews] = useState([]);
-  const url = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-03&sortBy=publishedAt&apiKey=3a1a4d9585284dc39458e3e0ffee368a`;
+  const url = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-10&sortBy=publishedAt&apiKey=3a1a4d9585284dc39458e3e0ffee368a`;
   useEffect(() => {
     axios.get(url)
     .then(res => {
       const newsData = res.data.articles;
-      console.log(newsData);
       setNews(newsData.slice(0,18))
     })
   })
